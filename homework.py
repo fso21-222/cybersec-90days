32d94fa2f6bc0ad4f04c53a1dce451e1694d369b
fso21-222 <g69641876@gmail.com>
2026-06-02 22:18:22 +0000

    Day2 - homework completed

---
+++ homework.py
@@ -0,0 +1,24 @@
+company_s = input ("Название")
+ip_2 = input ("Адрес")
+print ("[Info] начинаем сканирование компании "+ company_s + " на Ip-адресе " + ip_2 )
+
+
+
+name = input ("Name version:")
+print ("*************************************************************")
+version = "1.0.0"
+print ("Название:"+ name)
+print ("Версия:"+ version) 
+print ("*************************************************************")
+
+
+
+
+server_key = "Секретный ключ"
+user_token = "Обычный ключ"
+box = server_key
+box2 = user_token
+user_token = box
+server_key = box2
+print ("server_key:"+ server_key)
+print ("user_token:"+ user_token)
