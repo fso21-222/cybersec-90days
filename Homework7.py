762752693b6c37a804e299231438489efea65a42
fso21-222 <g69641876@gmail.com>
2026-06-08 16:12:00 +0000

    HOMEWORK7 - completed

---
+++ Homework7.py
@@ -0,0 +1,17 @@
+password = ["12345", "12344", "qwert" , "admin"]
+print ("Первый пароль" + str (password [0]))
+print ("Третий пароль" + str (password [2]))
+
+
+
+black_list = []
+b = input("Введите опасный адрес:")
+black_list.append(b)
+print ("ОПАСНЫЙ АДРЕС:" + str(black_list[0]))
+b = input("Введите опасный адрес:")
+black_list.append(b)
+print ("ОПАСНЫЙ АДРЕС 2:" + str(black_list[1]))
+print ("ОПАСНЫЕ АДРЕСА:" + str(black_list))
+
+
+
