25331a7bc7500750dd900fc755e4d232fc47e16d
fso21-222 <g69641876@gmail.com>
2026-06-19 22:43:40 +0000

    DAY - COMPLETED

---
+++ Homework11.py
@@ -0,0 +1,27 @@
+def welcome_hacker():
+ 	print ("Доступ получен.")
+welcome_hacker()
+print ("Добро пожаловать в систему ")
+welcome_hacker()
+print ("Элитный хакер!")
+welcome_hacker()
+
+
+
+
+def scan_network():
+	print("Запуск сканирования сети")	
+for u in range(1, 4):
+	print("Сканируем узел 10.0.0." +  str(u))
+	print("Сканирование завершено")
+scan_network()
+
+
+
+def raid_calculator():
+	raid = int(input("число стен:"))
+	raid2 = int(input("какое оружие бобы или взрыв:"))
+if sten > 2:
+	print("Для рейда нам понадобится:" + str(raid2))
+else:
+	print()
